052664d440edf7545f3e2c00fc9b77e975b9ae22
ManojChandran <manoj.meparambu@gmail.com>
2020-06-17 10:07:36 +0530

    updated detail on cognito

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -3,10 +3,16 @@
 resource "aws_api_gateway_rest_api" "MyDemoAPI" {
   name        = "MyDemoAPI"
   description = "This is my API for demonstration purposes"
+
+  tags = {
+    "Name"    = "Covid-Hackathon"
+    "Project" = "Terraform"
+  }
+
 }
 
 resource "aws_api_gateway_resource" "MyDemoResource" {
-  rest_api_id = "${aws_api_gateway_rest_api.MyDemoAPI.id}"
-  parent_id   = "${aws_api_gateway_rest_api.MyDemoAPI.root_resource_id}"
+  rest_api_id = "aws_api_gateway_rest_api.MyDemoAPI.id"
+  parent_id   = "aws_api_gateway_rest_api.MyDemoAPI.root_resource_id"
   path_part   = "mydemoresource"
 }
--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -2,4 +2,22 @@
 
 resource "aws_cognito_user_pool" "pool" {
   name = "mypool"
+
+  alias_attributes         = ["email"]
+  auto_verified_attributes = ["email"]
+
+  password_policy {
+    minimum_length    = 10
+    require_lowercase = false
+    require_numbers   = true
+    require_symbols   = false
+    require_uppercase = true
+
+    temporary_password_validity_days = 9
+  }
+
+  tags = {
+    "Name"    = "Covid-Hackathon"
+    "Project" = "Terraform"
+  }
 }
